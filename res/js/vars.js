06ba77f40d421eda0d994bd2a0a7713c8f4d24cb
cadriosula <cfa_44@yahoo.com>
2018-03-01 19:14:32 +0800

    Update vars.js

--- res/js/vars.js
+++ res/js/vars.js
@@ -1,14 +1,14 @@
- var url_home = "https://rawgit.com/poc-org/hrit/test";
- var url_image = "https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/";
- var url_mode2 = "https://rawgit.com/poc-org/hrit/test/learning_guide/pages/mode2page/modetwopage.html?page=";
+ var url_home = "https://rawgit.com/dxcuni/guidedlearn/itg";
+ var url_image = "https://raw.githubusercontent.com/dxcuni/guidedlearn/itg/res/images/";
+ var url_mode2 = "https://rawgit.com/dxcuni/guidedlearn/itg/pages/mode2page/modetwopage.html?page=";
  var mode2Image = "10.jpg";
     
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {    
-    $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
-    $scope.url_image = "https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/";
-    $scope.url_mode2 = "https://rawgit.com/poc-org/hrit/test/mod2page.html?page=";
+    $scope.url_home = "https://rawgit.com/dxcuni/guidedlearn/itg";
+    $scope.url_image = "https://raw.githubusercontent.com/dxcuni/guidedlearn/itg/res/images/";
+    $scope.url_mode2 = "https://rawgit.com/dxcuni/guidedlearn/itg/mod2page.html?page=";
     
     $scope.location = function (page) {
         var pages = JSON.parse(conf);
